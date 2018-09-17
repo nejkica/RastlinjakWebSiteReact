36026d5173d08cc3ef3d48ff9ad72c3222974831
Jernej Jevsevar <jevsevar@kookaburra.si>
2018-09-18 01:07:34 +0200

    prva objava

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import Logo from './logo';
+import seznamHrane from './seznamZaVsebino';
+import Meni from './meni';
+import Map from './map';
+import Navi from './navi';
+import gumbi from './gumbi';
 
 export default class App extends Component {
+  
   render() {
+    // console.log(seznamHrane);
     return (
-      <div>React simple starter</div>
+      <div className='app'>
+        <div className='divLogo'>
+          <Logo />
+        </div>
+        <div className='divOstalo'>
+          <Navi gumbi={ gumbi() } />
+          <Meni seznamHrane={ seznamHrane() } />
+          <Map />
+        </div>
+      </div>
     );
   }
 }
